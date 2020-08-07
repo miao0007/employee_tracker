7e2b0ec64fecf9055b293d9a7d5e46e478025049
miao0007 <miao188@hotmail.com>
2020-08-07 20:41:18 +0930

    test database connection

---
+++ index.js
@@ -0,0 +1,18 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const cTable = require("console.table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "huanXI861109",
+    database: "employee_trackerDB"
+});
+
+connection.connect(function(err){
+    if(err) throw err;
+    console.log("connection as id " + connection.threadId + "\n");
+    // start();
+});
+
